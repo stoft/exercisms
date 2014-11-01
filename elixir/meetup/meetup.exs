548fd468c363689198ac5c5c9295ca9c460b1f54
Rasmus Larsson <rasmus.larsson@gmail.com>
2014-11-02 00:25:39 +0100

    Cosmetic changes/indentation and slight renaming of variables/functions.

--- elixir/meetup/meetup.exs
+++ elixir/meetup/meetup.exs
@@ -7,11 +7,23 @@ defmodule Meetup do
       :monday | :tuesday | :wednesday
     | :thursday | :friday | :saturday | :sunday
 
-  @type schedule :: :first | :second | :third | :fourth | :last | :teenth
+  @type schedule :: :first | :second | :third 
+                  | :fourth | :last | :teenth
 
-  @days %{:monday => 1, :tuesday => 2, :wednesday => 3, :thursday => 4, :friday => 5, :saturday => 6, :sunday => 7}
+  @days %{:monday     => 1,
+          :tuesday    => 2,
+          :wednesday  => 3,
+          :thursday   => 4,
+          :friday     => 5,
+          :saturday   => 6,
+          :sunday     => 7}
 
-  @index %{:teenth => 0, :first => 0, :second => 1, :third => 2, :fourth => 3, :last => -1}
+  @index %{ :teenth => 0,
+            :first  => 0,
+            :second => 1,
+            :third  => 2,
+            :fourth => 3,
+            :last   => -1}
 
   @doc """
   Calculate a meetup date.
@@ -21,19 +33,20 @@ defmodule Meetup do
   """
   @spec meetup(pos_integer, pos_integer, weekday, schedule) :: :calendar.date
   def meetup(year, month, weekday, schedule) do
-    range = get_range(year, month, schedule)
-    day = get_day(year, month, weekday, range, @index[schedule])
+    day_range = get_day_range(year, month, schedule)
+    day = get_day(year, month, @days[weekday],
+      day_range, @index[schedule])
     {year, month, day}
   end
 
-  defp get_range(_year, _month, :teenth), do: 13..19
-  defp get_range(year, month, _schedule) do
+  defp get_day_range(_year, _month, :teenth), do: 13..19
+  defp get_day_range(year, month, _schedule) do
     1 .. :calendar.last_day_of_the_month(year, month)
   end
 
-  defp get_day(year, month, weekday, range, index) do
-    Enum.filter(range, fn(x) ->
-      :calendar.day_of_the_week(year, month, x) === @days[weekday] end)
+  defp get_day(year, month, weekday, day_range, index) do
+    Enum.filter(day_range, fn(x) ->
+      :calendar.day_of_the_week(year, month, x) == weekday end)
     |> Enum.at(index)
   end
 
