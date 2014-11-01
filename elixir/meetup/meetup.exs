4fd77ec9f0909953ff2e9928341ba997cb2edaf1
Rasmus Larsson <rasmus.larsson@gmail.com>
2014-11-02 00:33:27 +0100

    Further refactoring.

--- elixir/meetup/meetup.exs
+++ elixir/meetup/meetup.exs
@@ -18,12 +18,12 @@ defmodule Meetup do
           :saturday   => 6,
           :sunday     => 7}
 
-  @index %{ :teenth => 0,
-            :first  => 0,
-            :second => 1,
-            :third  => 2,
-            :fourth => 3,
-            :last   => -1}
+  @week_index %{ :teenth => 0, # edge case
+                 :first  => 0,
+                 :second => 1,
+                 :third  => 2,
+                 :fourth => 3,
+                 :last   => -1}
 
   @doc """
   Calculate a meetup date.
@@ -35,19 +35,19 @@ defmodule Meetup do
   def meetup(year, month, weekday, schedule) do
     day_range = get_day_range(year, month, schedule)
     day = get_day(year, month, @days[weekday],
-      day_range, @index[schedule])
+      day_range, @week_index[schedule])
     {year, month, day}
   end
 
-  defp get_day_range(_year, _month, :teenth), do: 13..19
+  defp get_day_range(_year, _month, :teenth), do: 13..19 #edge case
   defp get_day_range(year, month, _schedule) do
     1 .. :calendar.last_day_of_the_month(year, month)
   end
 
-  defp get_day(year, month, weekday, day_range, index) do
+  defp get_day(year, month, weekday, day_range, week_index) do
     Enum.filter(day_range, fn(x) ->
       :calendar.day_of_the_week(year, month, x) == weekday end)
-    |> Enum.at(index)
+    |> Enum.at(week_index)
   end
 
 end
