2c016655b53a9d6c95a2df2a50017c0896c16df8
Rasmus Larsson <rasmus.larsson@gmail.com>
2014-11-02 10:22:33 +0100

    Cosmetic changes and set Earth explicitly providing either correct values or failing hard.

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -3,25 +3,27 @@ defmodule SpaceAge do
   				| :mars | :jupiter
                 | :saturn | :neptune | :uranus
 
+  @earth_orbital_period_seconds 31557600
+
   @doc """
   Return the number of years a person that has 
   lived for 'seconds' seconds is aged on 'planet'. 
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-  	seconds / 31557600 / get_modifier(planet)
+  	seconds / @earth_orbital_period_seconds / get_modifier(planet)
   end
 
   def get_modifier(planet) do
   	case planet do
   		:mercury -> 0.2408467
-  		:venus -> 0.61519726
-  		:mars -> 1.8808158
+  		:venus   -> 0.61519726
+  		:mars    -> 1.8808158
   		:jupiter -> 11.862615
-  		:saturn -> 29.447498
+  		:saturn  -> 29.447498
   		:neptune -> 164.79132
-  		:uranus -> 84.016846
-  		_ -> 1 #Earth
+  		:uranus  -> 84.016846
+  		:earth 	 -> 1.0
   	end
   end
 end
