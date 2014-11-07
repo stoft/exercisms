40d28148bfde65d02d8c4b7c74578be0d5e7611e
Rasmus Larsson <rasmus.larsson@gmail.com>
2014-11-07 23:00:26 +0100

    Optimized version where base is only converted to char list and sorted once.

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,14 +4,20 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-	Enum.filter(candidates, 
-		&match_item(String.downcase(base), 
-			String.downcase(&1)))  
+    normalized_base = String.downcase(base) |> normalize
+    Enum.filter(candidates, 
+		  &match_item(String.downcase(base), 
+			 String.downcase(&1),
+       normalized_base))  
   end
-  defp match_item(base, candidate) when base === candidate, do: false
-  defp match_item(base, candidate) when length(base) != length(candidate), do: false
-  defp match_item(base, candidate) do
-  	Enum.sort(String.to_char_list(base)) === 
-  		Enum.sort(String.to_char_list(candidate))
+
+  defp match_item(same, same, _), do: false
+  defp match_item(base, candidate, _) when length(base) != length(candidate), do: false
+  defp match_item(_, candidate, normalized_base) do
+  	normalized_base === normalize(candidate)
+  end
+
+  defp normalize(string) do
+    string |> String.to_char_list |> Enum.sort
   end
 end
